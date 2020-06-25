e92875068c7b53605b289c3c6578ff7022ab7d8c
md islam <mdislam352935@yahoo.com>
2020-06-25 05:00:00 -0400

    BasicJavaProject Main method

---
+++ src/base/HellowWorld.java
@@ -0,0 +1,16 @@
+package base;
+
+public class HellowWorld {
+
+	public static void main(String[] args) {
+		// file:///C:/Program%20Files/Git/ReleaseNotes.html
+		
+		System.out.println("Hellow Java World");
+		
+		
+		
+		
+
+	}
+
+}
